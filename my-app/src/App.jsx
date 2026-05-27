5191e050ec8141ee1d3851c280a42f174bbd6a17
MysticAUZ <johndem000@gmail.com>
2026-05-27 19:25:45 +1000

    fixed redirect issues

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Homepage from "./components/Homepage";
+import Admin from './components/admin';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
