228405e348297212f812bbc43ced89fc1387e1fa
RyanUni123 <s8129454@live.vu.edu.au>
2026-06-03 17:11:11 +1000

    Updated App.jsx file
    Added navigation for additional pages

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,16 +2,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Homepage from "./components/Homepage";
-import Admin from './components/admin';
+import Admin from "./components/admin";
+import AddData from "./components/AddData";
+import ManageEmployees from "./components/ManageEmployees";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/homepage" element={<Homepage />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/add-data" element={<AddData />} /> 
+                <Route path="/manage-employees" element={<ManageEmployees />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
